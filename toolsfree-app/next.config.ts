5009d3cb966982a566f8a8f301cba931d74640ed
Ranjitkuyate <156173622+Ranjitkuyate@users.noreply.github.com>
2025-03-20 18:00:16 +0530

    Update next.config.ts

--- toolsfree-app/next.config.ts
+++ toolsfree-app/next.config.ts
@@ -1,1 +1,51 @@
+import { NextConfig } from 'next'
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  async headers() {
+    return [
+      {
+        source: '/sw.js',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=0, must-revalidate',
+          },
+          {
+            key: 'Service-Worker-Allowed',
+            value: '/',
+          },
+        ],
+      },
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY',
+          },
+          {
+            key: 'X-XSS-Protection',
+            value: '1; mode=block',
+          },
+        ],
+      },
+    ]
+  },
+  reactStrictMode: true,
+  swcMinify: true,
+  output: 'standalone',
+}
+
+export default nextConfig
 
