3919e95157fa71b8c79ca68396f9d487d76d7e69
Ranjitkuyate <156173622+Ranjitkuyate@users.noreply.github.com>
2025-03-20 12:28:49 +0000

    Update next.config.ts

--- toolsfree-app/next.config.ts
+++ toolsfree-app/next.config.ts
@@ -1,13 +1,1 @@
-import { NextConfig } from 'next'
 
-const nextConfig: NextConfig = {
-  /* config options here */
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-}
-
-export default nextConfig
